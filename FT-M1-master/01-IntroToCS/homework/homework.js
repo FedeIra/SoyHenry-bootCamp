da945e0c291bd5add540a1b0de3f5bf7372417fa
Federico Irarrazaval <fedeirar@gmail.com>
2022-07-18 19:10:21 -0300

    homework1

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,17 +1,29 @@
-'use strict'
+"use strict";
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  
+  let data = num.split("").reverse(), // con el split pasa a ser [1, 1, 0]. Con el reverse pasa a ser [0, 1, 1]
+    result = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    // igual a Math.pow(2, i) . Aca creo un  valor que va guardando los numeros del array a la potencia por su posicion
+    result += 2 ** i * data[i]; // aca agarra el valor de factor y lo multiplica por el valor del array y luego lo suma a result
+  }
+  return result;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let result = "";
 
+  while (num > 0) {
+    result = (num % 2) + result;
+    num = Math.floor(num / 2);
+  }
+  return result;
 }
-
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
-}
+};
