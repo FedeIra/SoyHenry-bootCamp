ceb4ff0c0c74e534a939814558e20592a012099d
Federico Irarrazaval <fedeirar@gmail.com>
2022-07-26 19:08:44 -0300

    pair programming7

--- FT-M1-master/07-Algoritmos-I/homework/homework.js
+++ FT-M1-master/07-Algoritmos-I/homework/homework.js
@@ -135,6 +135,19 @@ function selectionSort(array) {
   return array;
 }
 
+// OTRA SOLUCIÓN:
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    [array[i], array[min]] = [array[min], array[i]];
+  }
+  return array;
+}
 // No modificar nada debajo de esta línea
 // --------------------------------
 
