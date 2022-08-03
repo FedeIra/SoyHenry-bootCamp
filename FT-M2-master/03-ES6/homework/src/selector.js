804b7ed27f931c475fcdaec3a03de62cb8f21ede
Federico Irarrazaval <fedeirar@gmail.com>
2022-08-03 17:43:47 -0300

    pair programming3

--- FT-M2-master/03-ES6/homework/src/selector.js
+++ FT-M2-master/03-ES6/homework/src/selector.js
@@ -1,9 +1,3 @@
-/* COMENTARIOS DEL PRE REPASO:
-Resolver primero la funcion selectorTypeMatcher (la segunda).
-En segunda lugar, seguir con la tercera.
-Por último, la primera de todas.
-La idea es crear nuestra propia librería de selectores. Relacionado con queryselector que busca por nombre, clase o id, pero nosotros lo hacemos.
-No hay packaje jason. Para verlos hay que abrir el archivo specRuner.html en el homework. En el navegador ves los tests, no hay que hacer npm install ni nada de eso. DIVIDE ET IMPERA. */
 var traverseDomAndCollectElements = function (matchFunc, startEl) {
   var resultSet = [];
 
@@ -15,14 +9,31 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if (matchFunc(startEl)) {
+    resultSet.push(startEl);
+  }
+  for (let i = 0; i < startEl.children.length; i++) {
+    var collectedElements = traverseDomAndCollectElements(
+      matchFunc,
+      startEl.children[i]
+    );
+    resultSet = resultSet.concat(collectedElements);
+  }
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
-var selectorTypeMatcher = function (selector) {
+var selectorTypeMatcher = (selector) =>
   // tu código aquí
-};
+  selector[0] === "#"
+    ? "id"
+    : selector[0] === "."
+    ? "class"
+    : selector.includes(".")
+    ? "tag.class"
+    : "tag";
 
 // NOTA SOBRE LA FUNCIÓN MATCH
 // recuerda, la función matchFunction devuelta toma un elemento como un
@@ -31,11 +42,34 @@ var selectorTypeMatcher = function (selector) {
 
 var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
+
   var matchFunction;
+
   if (selectorType === "id") {
+    return (matchFunction = (el) => el.id && `#${el.id}` === selector);
   } else if (selectorType === "class") {
+    matchFunction = (el) => {
+      for (let clase = 0; clase < el.classList.length; clase++) {
+        if (`.${el.classList[clase]}` === selector) {
+          return true;
+        }
+      }
+      return false;
+    };
   } else if (selectorType === "tag.class") {
+    matchFunction = (el) => {
+      let classTag = selector.split("."),
+        tag = classTag[0],
+        clase = classTag[1];
+
+      return (
+        matchFunctionMaker(tag)(el) && matchFunctionMaker(`.${clase}`)(el)
+      ); /* Es la auto invocación con el parámetro necesario */
+    };
   } else if (selectorType === "tag") {
+    matchFunction = (el) => {
+      return el.tagName && el.tagName.toLowerCase() === selector;
+    };
   }
   return matchFunction;
 };
