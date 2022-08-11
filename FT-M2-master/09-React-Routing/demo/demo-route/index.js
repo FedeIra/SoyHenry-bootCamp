eb568fb1d76404cc8dfbb52d56a8685294df389b
Federico Irarrazaval <fedeirar@gmail.com>
2022-08-11 14:48:39 -0300

    react routing

--- FT-M2-master/09-React-Routing/demo/demo-route/index.js
+++ FT-M2-master/09-React-Routing/demo/demo-route/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Route, Switch, Link, HashRouter as Router, useRouteMatch } from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import {
+  Route,
+  Switch,
+  Link,
+  HashRouter as Router,
+  useRouteMatch,
+} from "react-router-dom";
 
 function NavBar() {
   return (
@@ -27,63 +33,62 @@ function NavBar() {
       <Link to="/Sensitive">Sensitive Mayus</Link>
     </div>
   );
-};
+}
 
-function Home() {
+function Home(props) {
+  console.log(props);
+
   return (
     <div>
       <h2>Home, Soy Henry!!</h2>
     </div>
   );
-};
-
+}
 
 const Root = (
   <Router>
     <NavBar />
-    <Switch>
-      <Route path="/home">
-        <Home />
-      </Route>
-      <Route exact path="/exact">
-        <h2>Exact</h2>
-      </Route>
-      {/* <Route path="/exact/another">
+
+    <Route path="/" render={(props) => <Home info={props} />}>
+      <Home />
+    </Route>
+    <Route exact path="/exact">
+      <h2>Exact</h2>
+    </Route>
+    {/* <Route path="/exact/another">
         <h2>Another</h2>
       </Route> */}
-      <Route path="/exact">
-        <h2>Not Exact</h2>
-      </Route>
-      {/* <Route path="/nostrict/">
+    <Route path="/exact">
+      <h2>Not Exact</h2>
+    </Route>
+    {/* <Route path="/nostrict/">
         <h2>Not Strict</h2>
       </Route> */}
-      <Route path="/nostrict">
-        <h2>Not Strict</h2>
-      </Route>
-      <Route strict path="/strict/">
-        <h2>Strict</h2>
-      </Route>
-      {/* <Route strict path="/strict">
-        <h2>Strict</h2>
-      </Route> */}
-      {/* <Route exact strict path="/strict">
+    <Route path="/nostrict">
+      <h2>Not Strict</h2>
+    </Route>
+    <Route strict path="/strict/">
+      <h2>Strict</h2>
+    </Route>
+    {/* <Route strict path="/strict">
         <h2>Strict</h2>
       </Route> */}
-      {/* <Route sensitive path="/sensitive">
+    {/* <Route exact strict path="/strict">
+        <h2>Strict</h2>
+      </Route> */}
+    {/* <Route sensitive path="/sensitive">
         <h2>Sensitive</h2>
       </Route>
       <Route sensitive path="/Sensitive">
         <h2>Sensitive Mayus</h2>
       </Route> */}
-      <Route path="/sensitive">
-        <h2>No Sensitive</h2>
-      </Route>
-      <Route path="/">
-        <h2>Default</h2>
-      </Route>
-    </Switch>
+    <Route path="/sensitive">
+      <h2>No Sensitive</h2>
+    </Route>
+    <Route path="/">
+      <h2>Default</h2>
+    </Route>
   </Router>
 );
 
-render(Root, document.querySelector('#app'));
-
+render(Root, document.querySelector("#app"));
