b7f406ecf16453a724b63a355a795ea0fe99a511
Federico Irarrazaval <fedeirar@gmail.com>
2022-08-12 00:17:55 -0300

    pair programming 8

--- FT-M2-master/09-React-Routing/homework/src/components/SearchBar.jsx
+++ FT-M2-master/09-React-Routing/homework/src/components/SearchBar.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 
-export default function SearchBar({onSearch}) {
+function SearchBar({ onSearch }) {
   const [city, setCity] = useState("");
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      onSearch(city);
-    }}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSearch(city);
+      }}
+    >
       <input
         type="text"
         placeholder="Ciudad..."
         value={city}
-        onChange={e => setCity(e.target.value)}
+        onChange={(e) => setCity(e.target.value)}
       />
       <input type="submit" value="Agregar" />
     </form>
   );
 }
+
+export default SearchBar;
