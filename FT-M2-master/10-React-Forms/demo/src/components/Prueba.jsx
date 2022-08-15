aa2d71b18616cb8ca125e75d55220f4379b7b4e8
Federico Irarrazaval <fedeirar@gmail.com>
2022-08-15 08:46:29 -0300

    exercises

---
+++ FT-M2-master/10-React-Forms/demo/src/components/Prueba.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div />;
+  }
+}
+
+// Cambia el código debajo de esta línea
+
+export default MyApp;
