f9cdbcd930887b6d9902c8202a7a4632f945901f
cameronmcnz <you@example.com>
2022-08-28 10:21:19 -0300

    homework advanced promises

--- FT-M3-master/04-AdvancePromises/homework/src/pledge.js
+++ FT-M3-master/04-AdvancePromises/homework/src/pledge.js
@@ -3,12 +3,20 @@
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+
+// const algo = new Promise(function(resolve,reject){
+//     if(1 === 1) resolve("valor")
+//     else reject("razon")
+// })
+
+// resolve("algo")
+
 function $Promise(executor) {
-  // if typeof executor is not a function return type error:
-  if (typeof executor !== 'function') {
-    throw new TypeError('executor must be a function');
-  }
+  if (typeof executor !== 'function')
+    throw new TypeError('executor is not a function');
+
   this._state = 'pending';
+  this._value = undefined;
   this._handlerGroups = [];
 
   executor(this._internalResolve.bind(this), this._internalReject.bind(this));
@@ -18,45 +26,51 @@ $Promise.prototype._internalResolve = function (data) {
   if (this._state === 'pending') {
     this._state = 'fulfilled';
     this._value = data;
-    this._callHandlers();
+    this._llamarHandlers();
   }
 };
 
-$Promise.prototype._internalReject = function (error) {
+$Promise.prototype._internalReject = function (data) {
   if (this._state === 'pending') {
     this._state = 'rejected';
-    this._value = error;
-    this._callHandlers();
+    this._value = data;
+    this._llamarHandlers();
   }
 };
 
-$Promise.prototype.then = function (successCb, errorCb) {
-  if (typeof successCb !== 'function') {
-    successCb = false;
-  }
-  if (typeof errorCb !== 'function') {
-    errorCb = false;
-  }
-  this._handlerGroups.push({
-    successCb,
-    errorCb,
-  });
+$Promise.prototype.then = function (successH, errorH) {
+  if (typeof successH !== 'function') successH = false;
+  if (typeof errorH !== 'function') errorH = null;
+  this._handlerGroups.push({ successCb: successH, errorCb: errorH });
+
   if (this._state !== 'pending') {
-    this._callHandlers();
+    this._llamarHandlers();
   }
 };
 
-$Promise.prototype._callHandlers = function () {
-  while (this._handlerGroups.length) {
-    let handle = this._handlerGroups.shift();
-    if (this._state === `fulfilled`) {
-      handle.successCb && handle.successCb(this._value);
+$Promise.prototype.catch = function (errH) {
+  // .catch(err => throw new Error(err))
+  this.then(null, errH);
+};
+
+$Promise.prototype._llamarHandlers = function () {
+  // handlers --> [ { successCb: e, errorCb: f }]
+  // promise.then(a,b).then(c,d).then(e,f)
+
+  //       [   ]
+
+  while (this._handlerGroups.length > 0) {
+    var handler = this._handlerGroups.shift(); // { successCb: false, errorCb: d }
+
+    if (this._state === 'fulfilled') {
+      handler.successCb && handler.successCb(this._value);
     } else {
-      handle.errorCb && handle.errorCb(this._value);
+      handler.errorCb && handler.errorCb(this._value);
     }
   }
 };
 
+module.exports = $Promise;
 /*-------------------------------------------------------
 El spec fue diseñado para funcionar con Test'Em, por lo tanto no necesitamos
 realmente usar module.exports. Pero aquí está para referencia:
